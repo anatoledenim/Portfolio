45a5e3bb9fa9c54ffcbbaee339d808caddb3f9f8
216120191125 <136864167+216120191125@users.noreply.github.com>
2024-06-18 17:36:39 +0200

    update(change color theme)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,18 +3,19 @@ import AnimatedTitle from '../../components/AnimatedTitle'
 import { ActiveContext } from '../../contexts/active.context'
 import { useContext, useEffect } from 'react'
 import ImagePresentation from '../../components/PresentationImage'
-import photo from '../../assets/IMG_1912.jpg'
+import photo from '../../assets/IMG_2915.jpg'
 import '../../styles/Home/Home.css'
 import PresentationText from '../../components/PresentationText'
 import WhoButton from '../../components/WhoButton'
-import CV from '../../assets/CV.pdf'
-import Cards from '../../components/Cards'
-import sass from '../../assets/sass_logo.png'
-import nodejs from '../../assets/nodejs-logo.png'
-import react from '../../assets/React-JS.png'
-import htmlcss from '../../assets/html_css_js.png'
+import CV from '../../assets/CV_DENIMAL_Anatole_developpeur_web.pdf'
+// import Cards from '../../components/Cards'
+// import sass from '../../assets/sass_logo.png'
+// import nodejs from '../../assets/nodejs-logo.png'
+// import react from '../../assets/React-JS.png'
+// import htmlcss from '../../assets/html_css_js.png'
+// import GameBoy from '../../assets/xkat9inz.bmp'
 
-const liens = [htmlcss, sass, react, nodejs]
+// const liens = [htmlcss, sass, react, nodejs]
 
 function Home() {
     const { toggleActive, isActive } = useContext(ActiveContext)
@@ -37,14 +38,14 @@ function Home() {
                 <ChangeTheme />
                 <WhoButton />
                 <div className="home-presentation">
-                    <div className="home-presentation-cards-div">
-                        <PresentationText content="Compétences téchniques :" />
+                    {/* <div className="home-presentation-cards-div">
+                        <PresentationText content="Stack téchniques :" />
                         <div className="home-presentation-cards">
                             {liens.map((lien) => (
                                 <Cards key={lien} picture={lien} />
                             ))}
                         </div>
-                    </div>
+                    </div> */}
                     <div className="home-presentation-div">
                         <a href={CV} target="_blank" rel="noreferrer">
                             <ImagePresentation
@@ -53,7 +54,7 @@ function Home() {
                             />
                         </a>
                         <a
-                            className="home-presentation-cv border-underline"
+                            className="home-presentation-cv border-underline-cv"
                             href={CV}
                             target="_blank"
                             rel="noreferrer"
@@ -61,7 +62,8 @@ function Home() {
                             Cliquez pour voir mon CV
                         </a>
                     </div>
-                    <PresentationText content="Je m'appelle Anatole, developpeur web full stack Junior ! J'ai effectué une reconversion dans le domaine du web qui m'a toujours passionné. Travaillant d'abord en autodidacte, j'ai ensuite décidé de poursuivre mon apprentissage à travers la formation dédiée d'Openclassrooms." />
+                    <PresentationText content="Je m'appelle Anatole, developpeur web full stack Junior ! J'ai effectué une reconversion dans le domaine du web qui m'a toujours passionné. Travaillant d'abord en autodidacte, j'ai ensuite décidé de poursuivre mon apprentissage à travers la formation dédiée d'Openclassrooms. Aujourd'hui diplomé, je saurais satisfaire vos attentes." />
+                    {/* <img src={GameBoy} alt="gameboy" /> */}
                 </div>
             </div>
         </div>
